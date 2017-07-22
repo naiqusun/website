14bd62772c835b685831bdfd8365f3ecc69f78c2
Naiqu Sun <naiqusun@Naiqus-MacBook-Air.local>
2017-07-21 22:00:09 -0700

    shipppppppy

--- APICallScripts.js
+++ APICallScripts.js
@@ -77,3 +77,105 @@ function lookUpGymClassReviewGET(search) {
     console.log(response)
   });
 }
+
+function lookUpAllGymClassGET() {
+  let settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://snshealthie.herokuapp.com/api/v2/classes/",
+    "method": "GET",
+    "headers": {
+      "authorization": "Token ced12082a40905503ad2ad29367aeafb824ed2b8",
+    }
+  }
+  console.log(settings.url)
+  $.ajax(settings).done(function (response) {
+    renderAllGymClassSearchResults(response)
+  });
+}
+
+
+function createNewGymClassReviewPOST(){
+  let form = new FormData(postClassReview);
+  for (var pair of form.entries()) {
+      console.log(pair[0]+ ', ' + pair[1]);
+  }
+
+  let settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://snshealthie.herokuapp.com/api/v2/reviews/",
+  "method": "POST",
+  "headers": {
+    "authorization": "Token ced12082a40905503ad2ad29367aeafb824ed2b8",
+  },
+  "processData": false,
+  "contentType": false,
+  "mimeType": "multipart/form-data",
+  "data": form
+  }
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  })
+}
+
+
+function createNewGymClassPOST() {
+  let form = new FormData(postClass);
+  for (var pair of form.entries()) {
+      console.log(pair[0]+ ', ' + pair[1]);
+  }
+  let settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://snshealthie.herokuapp.com/api/v2/classes/",
+    "method": "POST",
+    "headers": {
+      "authorization": "Token ced12082a40905503ad2ad29367aeafb824ed2b8",
+      },
+    "processData": false,
+    "contentType": false,
+    "mimeType": "multipart/form-data",
+    "data": form
+  }
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
+}
+
+
+
+
+function renderAllGymClassSearchResults(classResults) {
+  console.log(classResults);
+  var classResultsStart = document.getElementById("placeholderClassSearchResult");
+
+  for (i = 0; i < classResults.results.length; i++) {
+
+    var classResultsContainer = document.createElement("div");
+    var classResultsFront = document.createElement("div");
+    var classResultsBack = document.createElement("div");
+
+    classResultsContainer.classList.add('card');
+    classResultsContainer.classList.add('small');
+    classResultsFront.classList.add('card-content');
+    classResultsBack.classList.add("card-reveal");
+
+    classResultsStart.appendChild(classResultsContainer);
+    classResultsContainer.appendChild(classResultsFront);
+    classResultsContainer.appendChild(classResultsBack);
+
+    classResultsFront.innerHTML = '<span class="card-title activator grey-text text-darken-4">'+ classResults.results[i].name + '<i class="material-icons right">more_vert</i></span>'
+    + '<p>' + classResults.results[i].gym + '</p>'
+    // + '<p>' + classResults.classes[i].avgRating + '</p>'
+    // + '<p>' + classResults.classes[i].gym + '</p>'
+    // + '<p>' + classResults.classes[i].location + '</p>';
+    classResultsBack.innerHTML = '<span class="card-title grey-text text-darken-4">'+ 'More Info' + '<i class="material-icons right">close</i></span>'
+    // + '<p>' + classResults.results[i].type + '</p>'
+    // + '<p>' + classResults.classes[i].description + '</p>'
+    // + '<p>' + classResults.classes[i].dateOffered + '</p>'
+    // + '<p>' + classResults.classes[i].instructor + '</p>'
+    // + '<p>' + classResults.classes[i].duration + '</p>';
+
+  }
+}
